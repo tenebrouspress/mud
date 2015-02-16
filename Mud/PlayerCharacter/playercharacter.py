5ba775785035445de63f378a2612cc47cd2f4daf
Caleb Gutshall <cgutshal@gmail.com>
2015-02-15 19:25:41 -0500

    updated stats, and added readme for characters

--- Mud/PlayerCharacter/playercharacter.py
+++ Mud/PlayerCharacter/playercharacter.py
@@ -7,5 +7,9 @@ class playercharacter(object):
 
     def __init__(self):
         self.health = 0
+        #What is the role of trauma?
         self.trauma = 0
         self.name = None
+        self.weaponSkill = 0
+        self.strength = 0
+        
